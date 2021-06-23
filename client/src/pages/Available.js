2f2e6e453c6a42e0c34d93ecc8e21bca5b266b8b
James Yeates <jimibue@gmail.com>
2021-06-23 12:12:23 -0600

    availble UI normalized and hooked up

--- client/src/pages/Available.js
+++ client/src/pages/Available.js
@@ -1,67 +1,71 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import AgentProperties from '../components/AgentProperties';
 
-const agent = {
-    fullName: "Jimbo DUBO",
-    email: "test@test.com",
-    properties: [
-        {
-            street: "asd",
-            city: "slc",
-            zip: "84101",
-            sq_ft: 123,
-            beds: 12,
-            baths: 24,
-            price: 12341234
-        },
-        {
-            street: "asd",
-            city: "slc",
-            zip: "84101",
-            sq_ft: 123,
-            beds: 12,
-            baths: 24,
-            price: 12341234
-        }
-    ]
-};
-const agent1 = {
-    fullName: "Jimbo2 DUBO",
-    email: "test2@test.com",
-    properties: [
-        {
-            street: "2asd",
-            city: "slc",
-            zip: "84101",
-            sq_ft: 123,
-            beds: 12,
-            baths: 24,
-            price: 12341234
-        },
-        {
-            street: "asd",
-            city: "slc",
-            zip: "84101",
-            sq_ft: 123,
-            beds: 12,
-            baths: 24,
-            price: 12341234
-        },
-        {
-            street: "asasdfd",
-            city: "slc",
-            zip: "84101",
-            sq_ft: 123,
-            beds: 12,
-            baths: 24,
-            price: 12341234
-        }
-    ]
-};
-
-const agents = [agent, agent1]
-
 const Available = () => {
+    const [agents, setAgents] = useState([])
+    useEffect(()=>{
+        getAgentsProperties()
+    },{})
+
+    const normalizeData = (agentProperties) => {
+        // STEP 1 GET UNIQ IDS OF AGENTS
+            // Step 1a
+            // how do you go through my array of objects 
+            // and return array of agent_id =[1,1,2,2,]
+            // MAP
+
+             // Step 2a
+            // take array of numbers and create array of 
+            // uniq numbers
+            // [...new Set(x)]
+
+        const uniqAgentIDS =[...new Set(agentProperties.map(p=> p.agent_id))]
+
+        console.log(uniqAgentIDS)
+        const agentsData = []
+        // STEP 2 go through that array and match all my agents
+        // with  agentProperties array
+        uniqAgentIDS.forEach( id => {
+            // I have id of the agent
+            let properties = agentProperties.filter( ap => ap.agent_id === id)
+            console.log(properties)
+              // still in for each loop push each agent {fullName, email properties}
+             // object to agents array
+            let {agent_id, first_name, last_name, email} = properties[0]
+            // this step is not 100% necassary but removes unused keys from
+            // properties
+            const cleanedProperties = properties.map(p => {
+                return{street: p.street,
+                       zip:p.zip,
+                       state:p.state,
+                       city:p.city,
+                       sq_ft:p.sq_ft,
+                       beds:p.beds,
+                       baths:p.baths,
+                       price:p.price
+                    }
+            })
+
+            agentsData.push({
+                agent_id, fullName: `${first_name} ${last_name}`, 
+                email, 
+                properties: cleanedProperties
+            }
+            )
+        })
+        console.log(agentsData)
+        setAgents(agentsData)
+    }
+
+    const getAgentsProperties = async()=> {
+        try {
+            let res = await axios.get('/api/properties')
+            normalizeData(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <>
             <h1>Available</h1>
